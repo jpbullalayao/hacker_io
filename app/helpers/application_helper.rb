f7b216b8178fbd57603da911e036debd12a982c4
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2014-07-01 21:34:11 -0700

    Static pages progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis
+	def full_title(page_title)
+		base_title = "hacker.io"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} - #{base_title}"
+		end
+	end
 end
